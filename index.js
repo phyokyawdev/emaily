ab3bdd4be34ba7081b39145beff7cdf53233cb55
Phyo Kyaw <phyokyawdev@gmail.com>
2022-07-04 10:15:29 +0630

    add payment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 
 const keys = require("./config/keys");
 require("./models/User");
@@ -17,6 +18,7 @@ mongoose
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -28,6 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 // Heroku inject PORT env variable
 const PORT = process.env.PORT || 5000;
