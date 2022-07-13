a73ef9bf20d2ac6cc04bb8cbb3a0195bb957adde
Phyo Kyaw <phyokyawdev@gmail.com>
2022-07-13 15:46:31 +0630

    update landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-const Landing = () => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Emaily!</h1>
-      Collect feedback from your users
-    </div>
-  );
-};
+class Landing extends Component {
+  render() {
+    if (this.props.auth) {
+      return <Redirect to={"/surveys"} />;
+    }
 
-export default Landing;
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Emaily!</h1>
+        Collect feedback from your users
+        <div style={{ marginTop: "40px" }}>
+          <a href="/auth/google" class="waves-effect waves-light btn">
+            <i class="material-icons right">arrow_right</i>Start Here!
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Landing);
